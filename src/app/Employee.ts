aabb5c4d01e78ace27d4fb43272f122c6149224b
kalema1 <kalemar8@gmail.com>
2025-11-10 17:59:34 +0300

    add the html to the app component

---
+++ src/app/Employee.ts
@@ -0,0 +1,10 @@
+export class EmployeeModel {
+  empId!: number;
+  name!: string;
+  city!: string;
+  state!: string;
+  emailId!: string;
+  contactNo!: string;
+  address!: string;
+  pinCode!: string;
+}
